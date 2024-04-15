b5c53e5a2ced8edb485d73f863e7eb3dfcdbfc6a
Vladislav <jukov801@gmail.com>
2024-04-15 13:19:47 +0300

    Created a basic project with selection sort

---
+++ Sortings/main.cpp
@@ -0,0 +1,12 @@
+#include "Selection/selectionSort.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::vector<int> arr{1, 3, 2, 4, 7, 0};
+  sorts::selection_sort(arr);
+  
+  for(const auto& item : arr) {
+    std::cout << item << " ";
+  }
+  std::cout << std::endl;
+}
