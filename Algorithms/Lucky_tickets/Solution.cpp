3e3b6988ed75c625edcf8382dac606f40e7c7961
Vladislav <jukov801@gmail.com>
2024-09-26 21:25:33 +0300

    Added the main functionality of the algorithm in the m_ticks method

--- Algorithms/Lucky_tickets/Solution.cpp
+++ Algorithms/Lucky_tickets/Solution.cpp
@@ -22,8 +22,8 @@ long Solution::LuckyTikets::m_tickets(int digit) {
   case 0:
     return 1;
   case 1:
-    for (int a1 = 0; a1 < 10; ++a1) {
-      for (int b1 = 0; b1 < 10; ++b1) {
+    for (short a1 = 0; a1 < 10; ++a1) {
+      for (short b1 = 0; b1 < 10; ++b1) {
         if (a1 == b1) {
           ++count;
         }
@@ -31,10 +31,10 @@ long Solution::LuckyTikets::m_tickets(int digit) {
     }
     return count;
   case 2:
-    for (int a1 = 0; a1 < 10; ++a1) {
-      for (int a2 = 0; a2 < 10; ++a2) {
+    for (short a1 = 0; a1 < 10; ++a1) {
+      for (short a2 = 0; a2 < 10; ++a2) {
         int sumA = a1 + a2;
-        for (int b1 = 0; b1 < 10; ++b1) {
+        for (short b1 = 0; b1 < 10; ++b1) {
           int b2 = sumA - b1;
           if (b2 >= 0 && b2 < 10) {
             ++count;
@@ -44,12 +44,12 @@ long Solution::LuckyTikets::m_tickets(int digit) {
     }
     return count;
   case 3:
-    for (int a1 = 0; a1 < 10; ++a1) {
-      for (int a2 = 0; a2 < 10; ++a2) {
-        for (int a3 = 0; a3 < 10; ++a3) {
+    for (short a1 = 0; a1 < 10; ++a1) {
+      for (short a2 = 0; a2 < 10; ++a2) {
+        for (short a3 = 0; a3 < 10; ++a3) {
           int sumA = a1 + a2 + a3;
-          for (int b1 = 0; b1 < 10; ++b1) {
-            for (int b2 = 0; b2 < 10; ++b2) {
+          for (short b1 = 0; b1 < 10; ++b1) {
+            for (short b2 = 0; b2 < 10; ++b2) {
               int b3 = sumA - b1 - b2;
               if (b3 >= 0 && b3 < 10) {
                 ++count;
@@ -61,14 +61,14 @@ long Solution::LuckyTikets::m_tickets(int digit) {
     }
     return count;
   case 4:
-    for (int a1 = 0; a1 < 10; ++a1) {
-      for (int a2 = 0; a2 < 10; ++a2) {
-        for (int a3 = 0; a3 < 10; ++a3) {
-          for (int a4 = 0; a4 < 10; ++a4) {
+    for (short a1 = 0; a1 < 10; ++a1) {
+      for (short a2 = 0; a2 < 10; ++a2) {
+        for (short a3 = 0; a3 < 10; ++a3) {
+          for (short a4 = 0; a4 < 10; ++a4) {
             int sumA = a1 + a2 + a3 + a4;
-            for (int b1 = 0; b1 < 10; ++b1) {
-              for (int b2 = 0; b2 < 10; ++b2) {
-                for (int b3 = 0; b3 < 10; ++b3) {
+            for (short b1 = 0; b1 < 10; ++b1) {
+              for (short b2 = 0; b2 < 10; ++b2) {
+                for (short b3 = 0; b3 < 10; ++b3) {
                   int b4 = sumA - b1 - b2 - b3;
                   if (b4 >= 0 && b4 < 10) {
                     ++count;
@@ -82,16 +82,16 @@ long Solution::LuckyTikets::m_tickets(int digit) {
     }
     return count;
   case 5:
-    for (int a1 = 0; a1 < 10; ++a1) {
-      for (int a2 = 0; a2 < 10; ++a2) {
-        for (int a3 = 0; a3 < 10; ++a3) {
-          for (int a4 = 0; a4 < 10; ++a4) {
-            for (int a5 = 0; a5 < 10; ++a5) {
+    for (short a1 = 0; a1 < 10; ++a1) {
+      for (short a2 = 0; a2 < 10; ++a2) {
+        for (short a3 = 0; a3 < 10; ++a3) {
+          for (short a4 = 0; a4 < 10; ++a4) {
+            for (short a5 = 0; a5 < 10; ++a5) {
               int sumA = a1 + a2 + a3 + a4 + a5;
-              for (int b1 = 0; b1 < 10; ++b1) {
-                for (int b2 = 0; b2 < 10; ++b2) {
-                  for (int b3 = 0; b3 < 10; ++b3) {
-                    for (int b4 = 0; b4 < 10; ++b4) {
+              for (short b1 = 0; b1 < 10; ++b1) {
+                for (short b2 = 0; b2 < 10; ++b2) {
+                  for (short b3 = 0; b3 < 10; ++b3) {
+                    for (short b4 = 0; b4 < 10; ++b4) {
                       int b5 = sumA - b1 - b2 - b3 - b4;
                       if (b5 >= 0 && b5 < 10) {
                         ++count;
@@ -107,18 +107,18 @@ long Solution::LuckyTikets::m_tickets(int digit) {
     }
     return count;
   case 6:
-    for (int a1 = 0; a1 < 10; ++a1) {
-      for (int a2 = 0; a2 < 10; ++a2) {
-        for (int a3 = 0; a3 < 10; ++a3) {
-          for (int a4 = 0; a4 < 10; ++a4) {
-            for (int a5 = 0; a5 < 10; ++a5) {
-              for (int a6 = 0; a6 < 10; ++a6) {
+    for (short a1 = 0; a1 < 10; ++a1) {
+      for (short a2 = 0; a2 < 10; ++a2) {
+        for (short a3 = 0; a3 < 10; ++a3) {
+          for (short a4 = 0; a4 < 10; ++a4) {
+            for (short a5 = 0; a5 < 10; ++a5) {
+              for (short a6 = 0; a6 < 10; ++a6) {
                 int sumA = a1 + a2 + a3 + a4 + a5 + a6;
-                for (int b1 = 0; b1 < 10; ++b1) {
-                  for (int b2 = 0; b2 < 10; ++b2) {
-                    for (int b3 = 0; b3 < 10; ++b3) {
-                      for (int b4 = 0; b4 < 10; ++b4) {
-                        for (int b5 = 0; b5 < 10; ++b5) {
+                for (short b1 = 0; b1 < 10; ++b1) {
+                  for (short b2 = 0; b2 < 10; ++b2) {
+                    for (short b3 = 0; b3 < 10; ++b3) {
+                      for (short b4 = 0; b4 < 10; ++b4) {
+                        for (short b5 = 0; b5 < 10; ++b5) {
                           int b6 = sumA - b1 - b2 - b3 - b4 - b5;
                           if (b6 >= 0 && b6 < 10) {
                             ++count;
@@ -136,10 +136,115 @@ long Solution::LuckyTikets::m_tickets(int digit) {
     }
     return count;
   case 7:
+    for (short a1 = 0; a1 < 10; ++a1) {
+      for (short a2 = 0; a2 < 10; ++a2) {
+        for (short a3 = 0; a3 < 10; ++a3) {
+          for (short a4 = 0; a4 < 10; ++a4) {
+            for (short a5 = 0; a5 < 10; ++a5) {
+              for (short a6 = 0; a6 < 10; ++a6) {
+                for (short a7 = 0; a7 < 10; ++a7) {
+                  int sumA = a1 + a2 + a3 + a4 + a5 + a6 + a7;
+                  for (short b1 = 0; b1 < 10; ++b1) {
+                    for (short b2 = 0; b2 < 10; ++b2) {
+                      for (short b3 = 0; b3 < 10; ++b3) {
+                        for (short b4 = 0; b4 < 10; ++b4) {
+                          for (short b5 = 0; b5 < 10; ++b5) {
+                            for (short b6 = 0; b6 < 10; ++b6) {
+                              int b7 = sumA - b1 - b2 - b3 - b4 - b5 - b6;
+                              if (b7 >= 0 && b7 < 10) {
+                                ++count;
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
     return count;
   case 8:
+    for (short a1 = 0; a1 < 10; ++a1) {
+      for (short a2 = 0; a2 < 10; ++a2) {
+        for (short a3 = 0; a3 < 10; ++a3) {
+          for (short a4 = 0; a4 < 10; ++a4) {
+            for (short a5 = 0; a5 < 10; ++a5) {
+              for (short a6 = 0; a6 < 10; ++a6) {
+                for (short a7 = 0; a7 < 10; ++a7) {
+                  for (short a8 = 0; a8 < 10; ++a8) {
+                    int sumA = a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8;
+                    for (short b1 = 0; b1 < 10; ++b1) {
+                      for (short b2 = 0; b2 < 10; ++b2) {
+                        for (short b3 = 0; b3 < 10; ++b3) {
+                          for (short b4 = 0; b4 < 10; ++b4) {
+                            for (short b5 = 0; b5 < 10; ++b5) {
+                              for (short b6 = 0; b6 < 10; ++b6) {
+                                for (short b7 = 0; b7 < 10; ++b7) {
+                                  int b8 = sumA - b1 - b2 - b3 - b4 - b5 - b6 - b7;
+                                  if (b8 >= 0 && b8 < 10) {
+                                    ++count;
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
     return count;
   case 9:
+    for (short a1 = 0; a1 < 10; ++a1) {
+      for (short a2 = 0; a2 < 10; ++a2) {
+        for (short a3 = 0; a3 < 10; ++a3) {
+          for (short a4 = 0; a4 < 10; ++a4) {
+            for (short a5 = 0; a5 < 10; ++a5) {
+              for (short a6 = 0; a6 < 10; ++a6) {
+                for (short a7 = 0; a7 < 10; ++a7) {
+                  for (short a8 = 0; a8 < 10; ++a8) {
+                    for (short a9 = 0; a9 < 10; ++a9) {
+                      int sumA = a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9;
+                      for (short b1 = 0; b1 < 10; ++b1) {
+                        for (short b2 = 0; b2 < 10; ++b2) {
+                          for (short b3 = 0; b3 < 10; ++b3) {
+                            for (short b4 = 0; b4 < 10; ++b4) {
+                              for (short b5 = 0; b5 < 10; ++b5) {
+                                for (short b6 = 0; b6 < 10; ++b6) {
+                                  for (short b7 = 0; b7 < 10; ++b7) {
+                                    for (short b8 = 0; b8 < 10; ++b8) {
+                                      int b9 = sumA - b1 - b2 - b3 - b4 - b5 - b6 - b7 - b8;
+                                      if (b9 >= 0 && b9 < 10) {
+                                        ++count;
+                                      }
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
     return count;
   default:
     std::cout << "Incorrect data!" << std::endl;
