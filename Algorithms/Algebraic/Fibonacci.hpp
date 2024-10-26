c2bb771f84b4e63a226083659f823b502b315320
Vladislav <jukov801@gmail.com>
2024-10-26 16:58:59 +0300

    Added 3 Fibonacci counting algorithms

--- Algorithms/Algebraic/Fibonacci.hpp
+++ Algorithms/Algebraic/Fibonacci.hpp
@@ -1,8 +1,29 @@
 #ifndef F8DB5878_8B45_4651_95E5_D14234517F0D
 #define F8DB5878_8B45_4651_95E5_D14234517F0D
 
-namespace Algorithms {
+#include <cmath>
 
+namespace Algorithms {
+  uint64_t FibonacciRecursive(int n) {
+    if (n == 1 || n == 2) return 1;
+    return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+  }
+
+  uint64_t FibonacciIterative(int n) {
+    uint64_t f1 = 1;
+    uint64_t f2 = 1;
+    for(int i = 2; i < n; ++i) {
+      uint64_t f3 = f1 + f2;
+      f1 = f2;
+      f2 = f3;
+    }
+    return f2;
+  }
+
+  uint64_t FibonacciGold(int n) {
+    double fi = (1.0 + std::sqrt(5)) / 2.0;
+    return static_cast<uint64_t>(std::floor(std::pow(fi, n) / std::sqrt(5) + 1.0 / 2.0));
+  }
 }
 
 #endif /* F8DB5878_8B45_4651_95E5_D14234517F0D */
--- Algorithms/Algebraic/main.cpp
+++ Algorithms/Algebraic/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "Fibonacci.hpp"
 
 int main(int argc, char** argv) {
+  std::cout << "Recursive: " << Algorithms::FibonacciRecursive(5) << std::endl;
+  std::cout << "Iterative: " << Algorithms::FibonacciIterative(5) << std::endl;
+  std::cout << "Gold: " << Algorithms::FibonacciGold(5) << std::endl;
 
   return EXIT_SUCCESS;
 }
