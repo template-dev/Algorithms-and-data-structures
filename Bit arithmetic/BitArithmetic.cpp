09474b140d3eb7f93ad9d0c13852793777a6ffad
Vladislav <jukov801@gmail.com>
2025-01-09 01:30:15 +0300

    Implemented the bit_count_cache_v1 function

--- Bit arithmetic/BitArithmetic.cpp
+++ Bit arithmetic/BitArithmetic.cpp
@@ -20,4 +20,16 @@ namespace BitArithmetic {
     }
     return count;
   }
+
+  int32_t bit_count_cache_v1(int32_t num) {
+    constexpr int32_t N = 256;
+
+    if (num < 0 || num > N)
+      return -1;
+
+    int32_t cache[N]{};
+    for (int32_t i = 0; i < N; ++i)
+      cache[i] = bit_count_first(i);
+    return cache[num];
+  }
 }
