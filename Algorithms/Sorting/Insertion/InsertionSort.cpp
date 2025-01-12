1b3a3cb7f74be40c9b8966508ca4632e54f69288
Vladislav <jukov801@gmail.com>
2025-01-12 03:04:15 +0300

    Added an insertion sort

---
+++ Algorithms/Sorting/Insertion/InsertionSort.cpp
@@ -0,0 +1,7 @@
+#include "InsertionSort.hpp"
+
+void Sort::Insertion(std::vector<int>& vec) {
+  for (size_t i = 1; i < vec.size(); ++i)
+    for (size_t j = i - 1; j >= 0 && (vec[j] > vec[j + 1]); --j)
+      std::swap(vec[j], vec[j + 1]);
+}
---
+++ Algorithms/Sorting/Insertion/InsertionSort.hpp
@@ -0,0 +1,10 @@
+#ifndef A8E7BF63_5DB4_4B2C_97D6_7633AD6FBC08
+#define A8E7BF63_5DB4_4B2C_97D6_7633AD6FBC08
+
+#include <vector>
+
+namespace Sort {
+  void Insertion(std::vector<int>& vec);
+}
+
+#endif /* A8E7BF63_5DB4_4B2C_97D6_7633AD6FBC08 */
