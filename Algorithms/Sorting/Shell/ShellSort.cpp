7ca09b9ec40ab8927e0a7bba939225c00e151989
Vladislav <jukov801@gmail.com>
2025-01-24 01:21:15 +0300

    Fixed a minor bug

--- Algorithms/Sorting/Shell/ShellSort.cpp
+++ Algorithms/Sorting/Shell/ShellSort.cpp
@@ -3,6 +3,6 @@
 void Sort::Shell(std::vector<int>& vec) {
     for (size_t gap = vec.size() / 2; gap > 0; gap /= 2)
         for (size_t i = gap; i < vec.size(); ++i)
-            for (size_t j = i; j >= gap && vec[j] < vec[j - 1]; j -= gap)
+            for (size_t j = i; j >= gap && vec[j] < vec[j - gap]; j -= gap)
                 std::swap(vec[j], vec[j - gap]);
 }
